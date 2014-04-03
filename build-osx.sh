cfd46fffd4ddbc9776e6e84f8203baac7dff2638
David Turner <csusbdt@gmail.com>
2014-04-03 08:35:42 -0700

    X

--- build-osx.sh
+++
@@ -1,8 +0,0 @@
-clang++ `sdl2-config --libs --cflags`  \
-        -I/usr/local/include           \
-        -L/usr/local/lib               \
-        -llua                          \
-        -lSDL2_image                   \
-        -lSDL2_ttf                     \
-        -llua                          \
-        src/*.cpp
--- scripts/main.lua
+++
@@ -1,4 +0,0 @@
-setbg("assets/star-map.jpg")
-dofile("scripts/update.lua")
-print("main.lua running.")
-
