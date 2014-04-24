6bb7d4b041423a62c27ceebf8923f9c00e588d45
David Turner <csusbdt@gmail.com>
2014-04-24 12:15:35 -0700

    X

---
+++ build.sh
@@ -0,0 +1,2 @@
+clang `sdl2-config --libs --cflags` -I /usr/local/include -llua src/*.c
+
---
+++ src/global.h
@@ -0,0 +1,15 @@
+#ifndef APP_GLOBAL_H
+#define APP_GLOBAL_H
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+#include "SDL.h"
+
+#endif
+
