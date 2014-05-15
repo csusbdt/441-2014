cf1886f0fc2b098cf03dddfb2aa47f39e2e9809a
David Turner <csusbdt@gmail.com>
2014-05-15 10:50:52 -0700

    X

--- working/scripts/init.lua
+++ working/scripts/init.lua
@@ -1,30 +1,7 @@
---[[
-local Button = require('app.Button')
-local waves = require('res.waves')
-local fonts = require('res.fonts')
-
-local b1 = Button.create_from_text('Exit', 100, 60)
-local b2 = Button.create_from_text('Play door sound', 100, 260)
-local b3 = Button.create_from_text('Stop music', 300, 60)
-local b4 = Button.create_from_file('textures/ima.bmp', 300, 260)
-
-local door = waves.get('waves/door.wav')
-local musicloop = waves.get('music/menu.wav'):loop()
-
-function on_draw()
-	b1:draw()
-	b2:draw()
-	b3:draw()
-	b4:draw()
-end
-
-function on_touch(x, y) 
-	if b1:contains(x, y) then quit() end
-	if b2:contains(x, y) then door:play() end
-	if b3:contains(x, y) then musicloop:stop() end
-end
---]]
+app = {
+	dialog_x = 30,
+	dialog_y = 400
+}
 
 require('title.screen').show()
---require('story.screen').show()
 
--- working/scripts/story/screen.lua
+++ working/scripts/story/screen.lua
@@ -1,34 +1,45 @@
 _ENV = {
 	_G		= _G,
 	require 	= require,
-	collectgarbage	= collectgarbage
+	collectgarbage	= collectgarbage,
+	dx		= app.dialog_x,
+	dy		= app.dialog_y
 }
 
 Button = require('app.Button')
+textures = require('res.textures')
+dialog_font = require('res.fonts').get('dialog')
 waves  = require('res.waves')
 
 function on_draw()
 	b1:draw()
+	ima:draw()
+	if dialog then dialog:draw(dx, dy) end
 end
 
 function on_touch(x, y) 
 	if b1:contains(x, y) then 
-		exit()
+		hide()
 		require('title.screen').show()
+	elseif ima:contains(x, y) then 
+		dialog = textures.text('You clicked me.', dialog_font)
 	end
 end
 
 function show()
 	b1 = Button.create_from_text('Back to Title', 50, 30)
+	ima = Button.create_from_file('textures/ima.bmp', 300, 260)
 	music = waves.get('music/MushroomForest.wav'):loop()
 	_G.on_draw  = on_draw
 	_G.on_touch = on_touch
 end
 
-function exit()
+function hide()
 	music:stop() 
 	music = nil
 	b1 = nil
+	ima = nil
+	dialog = nil
 	_G.on_draw  = nil
 	_G.on_touch = nil
 	collectgarbage()
